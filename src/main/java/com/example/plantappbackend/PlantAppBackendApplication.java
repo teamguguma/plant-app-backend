ae389947a34f3cde30123c5d8b5dde95a6b0d2e9
Bogyeong Kim <bogyeongkim614@gmail.com>
2024-12-04 03:50:44 +0900

    feat: 기본파일 업로드

---
+++ src/main/java/com/example/plantappbackend/PlantAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.plantappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantAppBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlantAppBackendApplication.class, args);
+    }
+
+}
